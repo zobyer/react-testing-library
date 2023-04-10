488977a95dae8360e09225d5c153be5bf2a0b54d
zobyer <ahmzobyer@gmail.com>
2023-04-10 22:43:20 +0600

    test: quiz 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 
 function App() {
   const [buttonColor, setButtonColor] = useState("blue");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
   const newButtonColor = buttonColor === "blue" ? "red" : "blue";
 
   return (
@@ -11,9 +13,15 @@ function App() {
       <button
         style={{ backgroundColor: buttonColor }}
         onClick={() => setButtonColor(newButtonColor)}
+        disabled={isButtonDisabled}
       >
         Change color to {buttonColor}
       </button>
+
+      <input
+        type="checkbox"
+        onClick={(e) => setIsButtonDisabled(e.target.checked)}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -13,3 +13,18 @@ test("button has correct initial color", () => {
 
   expect(colorButton.textContent).toBe("Change color to red");
 });
+
+test("testing checkbox with buttons", () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole("button", {
+    name: "Change color to blue",
+  });
+  fireEvent.click(colorButton);
+
+  const checkBox = screen.getByRole("checkbox");
+  expect(checkBox).not.toBeChecked();
+
+  fireEvent.click(checkBox);
+  expect(colorButton).toBeDisabled();
+});
